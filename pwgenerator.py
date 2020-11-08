730546e9f1f680f3b6504b42d81ab6ed4da6b36d
Ivca Jelinkova <jelinkova.ivana@gmail.com>
2020-11-08 08:41:06 +0100

    generátor hesel

---
+++ pwgenerator.py
@@ -0,0 +1,101 @@
+# pwgenerator.py
+
+# Tvojou ulohou je vyrobit jednoduchy generator nahodnych hesiel
+# Znakova sada je postavena na znakoch ASCII s decimalnym indexom 33-90 a
+# 97-126.
+# Program by sa mal uzivatela spytat kolko hesiel by chcel vygenerovat a ake
+# dlhe by mali byt. Zaroven by sa mal spytat, ake zlozenie hesla si uzivatel
+# predstavuje.
+#
+# Priklad vystupu:
+# ===============
+# Dlzka hesla: 8
+# pocet hesiel: 3
+# Zlozenie
+#   1) len pismena
+#   2) pismena a cisla
+#   3) pismena + cisla + specialne znaky
+# Volba zlozenia: 2
+#
+# aBie2aep
+# ohvae8Xe
+# yiez3oV0
+# podmínkou je: z každé sady min. 1 znak
+
+
+import random
+
+mala = 'abcdefghijklmnopqrstuvwxyz'
+velka = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+cisla = '0123456789'
+spec_znaky = '!"#$%&\'()*+,-./:;<>=?@{}|~[]\\^_¨'
+
+
+
+def vyber_znak(sada) -> str:
+    """Vybírá konkrétní znak ze zadané sady dané globální proměnné mala, velka apod. """
+    return random.choice(sada)
+
+
+
+
+def main(delka_hesla, pocet_hesel, slozeni) -> str:
+    """Vygeneruje zadaný počet hesel dané délky a složení.
+    Podmínkou je vybrat z každé sady min. 1 znak. """
+    for pocet in range(pocet_hesel):
+        heslo = []
+        if slozeni == 1:
+            heslo.append(vyber_znak(mala))
+            heslo.append(vyber_znak(velka))
+
+            for i in range(delka_hesla-2):
+                sada = random.choice([mala, velka])
+                heslo.append(vyber_znak(sada))
+
+        elif slozeni == 2:
+            heslo.append(vyber_znak(mala))
+            heslo.append(vyber_znak(velka))
+            heslo.append(vyber_znak(cisla))
+
+            for i in range(delka_hesla-3):
+                sada = random.choice([mala, velka, cisla])
+                heslo.append(vyber_znak(sada))
+
+        elif slozeni == 3:
+            heslo.append(vyber_znak(mala))
+            heslo.append(vyber_znak(velka))
+            heslo.append(vyber_znak(cisla))
+            heslo.append(vyber_znak(spec_znaky))
+
+            for i in range(delka_hesla-4):
+                sada = random.choice([mala, velka, cisla, spec_znaky])
+                heslo.append(vyber_znak(sada))
+
+
+        else:
+            print('Zadej 1, 2 nebo 3. ')
+            slozeni = int(input('''Zadej číslo 1–3 pro výběr složení: 
+                                            1) pouze písmena,
+                                            2) písmena a čísla, 
+                                            3) písmena, čísla a spec. znaky. 
+                Vybrané složení → '''))
+        random.shuffle(heslo)
+        print("".join(heslo))
+
+
+if __name__ == "__main__":
+    delka_hesla = int(input('Délka hesla: '))
+    pocet_hesel = int(input('Počet hesel: '))
+    slozeni = int(input('''Zadej číslo 1–3 pro výběr složení: 
+                                1) pouze písmena,
+                                2) písmena a čísla, 
+                                3) písmena, čísla a spec. znaky. 
+        Vybrané složení → '''))
+
+    main(delka_hesla, pocet_hesel, slozeni)
+
+
+
+
+
+
