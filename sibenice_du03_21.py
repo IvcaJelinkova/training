0b4627371124abb8ec59598e817ab775ee6911fe
ivana.jelinkova <jelinkova.ivana@gmail.com>
2022-07-19 15:47:51 +0200

    add new file for a game named Sibenice

---
+++ sibenice_du03_21.py
@@ -0,0 +1,21 @@
+#sibenice.py
+
+# rozdel projekt do funkci a modulu s hezkymi jmény, piš testy a dokumentační řetězce,
+# funkční kousky dávej postupně do Gitu.
+
+#  - Počítač náhodně zvolí slovo (trávník, stromek, stavení). malá písmena a bez opakujících se písmen (čokoláda).
+
+#  - Výchozí stav je řetězec s tolika podtržítky, kolik je ve vybraném slově písmen.
+#  - Výchozí počet neúspěšných pokusů je nula.
+# Stále dokola:
+    #  - Zeptej se hráče na písmeno.
+    #  - Pokud je to písmeno ve vybraném slově, zaměň ve stavu příslušná podtržítka
+            # za ono písmeno. (Bude se hodit řetězcová metoda index. a funkce zamen.)
+    #  - Pokud dané písmeno není ve vybraném slově, započítej neúspěšný pokus.
+    #  - Vypiš stav (slovo s případnými podtržítky).
+    #  - Pokud už ve slově nejsou podtržítka, pogratuluj hráči a ukonči hru.
+    #  - Vypiš počet neúspěšných pokusů a odpovídající obrázek. Funkci, která ti vrátí
+    #       obrázek podle počtu pokusů, si můžeš stáhnout z Gitu
+    #  - Pokud je počet neúspěšných pokusů 9 (nebo víc), hráč prohrál. Dej mu to najevo a ukonči program.
+
+
