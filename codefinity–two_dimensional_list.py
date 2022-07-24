38cb65a21ede9a4e5bedc89cf55c79071791ba3d
ivjel <jelinkova.ivana@gmail.com>
2022-07-24 15:32:17 +0200

    iteration over dict keys

--- codefinity–two_dimensional_list.py
+++ codefinity–two_dimensional_list.py
@@ -54,4 +54,22 @@ for i in range(len(people)):
     print('Name:', people[i])
 
 
+# 5) Iterate over people_dict keys to output the following information – name, age, height:
+# People dictionary
+people_dict = {'Alex': (23, 178), 'Noah': (34, 189), 'Peter': (29, 175)}
+people_dict["John"] = (41, 185)
+people_dict["Michelle"] = (35, 165)
+
+# Construct for loop to print information about people
+for i in people_dict.keys():
+  print("Name:", i)
+  print("Age:", people_dict[i][0])
+  print("Height:", people_dict[i][1])
+  # Please, do not delete the delimiter below
+  print("---------")
+
+
+
+
+
 
