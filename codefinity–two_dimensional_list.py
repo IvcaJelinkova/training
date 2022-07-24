25abe6e924bb9ebe63a9d1ab10f986d2b7099e75
ivjel <jelinkova.ivana@gmail.com>
2022-07-24 13:55:51 +0200

    two-dimensional list

---
+++ codefinity–two_dimensional_list.py
@@ -0,0 +1,16 @@
+"""
+Codefinity:
+Recreate list (name as people) with people names and ages into two-dimensional. Note, that two entries are missing (these are Alex and Peter)
+Print the second element of the list.
+Print age of the fifth person.
+"""
+
+# Recreate list
+people = [["Alex", 23], ["Noah", 34], ["Peter", 29], ["John", 41],
+["Michelle", 35]]
+
+# Print the second element of the list
+print(people[1])
+
+# Print age of the fifth person
+print(people[4][1])
