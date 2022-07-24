4bab21ab3555affd263418691c1142b2f3313f3a
ivjel <jelinkova.ivana@gmail.com>
2022-07-24 14:10:01 +0200

    two-dimensional-tuple

--- codefinity–two_dimensional_list.py
+++ codefinity–two_dimensional_list.py
@@ -5,12 +5,22 @@ Print the second element of the list.
 Print age of the fifth person.
 """
 
-# Recreate list
+# 1) Recreate list in two-dimensional
 people = [["Alex", 23], ["Noah", 34], ["Peter", 29], ["John", 41],
 ["Michelle", 35]]
-
 # Print the second element of the list
 print(people[1])
-
 # Print age of the fifth person
 print(people[4][1])
+
+
+
+# 2) Create two-dimensional tuple
+two_d_tuple = (('Alex', 23, 178), ('Noah', 34, 189), ('Peter', 29, 175),
+('John', 41, 185), ('Michelle', 35, 165))
+# Information about Peter
+print(two_d_tuple[2])
+# Height of John
+print(two_d_tuple[3][2])
+# Print converted into list tuple
+print(list(two_d_tuple))
