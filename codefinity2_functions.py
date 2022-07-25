adb886bff0b9000fc8fa41b7974be4414eef049a
ivana.jelinkova <jelinkova.ivana@gmail.com>
2022-07-25 09:16:58 +0200

    upgrade

---
+++ codefinity2_functions.py
@@ -0,0 +1,14 @@
+#codefinity2_functions.py
+
+# 1) f-string: 
+# Write your code below
+def function():
+    capital = 'London'
+    print(f'{capital} is the capital and largest city of England and the United Kingdom')
+
+
+# Testing
+function()
+
+
+
