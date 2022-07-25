79d9bd4564cb4c3bd07a5f0c5a306d3d6b5e74b1
ivjel <jelinkova.ivana@gmail.com>
2022-07-25 21:22:46 +0200

    data_types-format_method-scientific_notation-boolean-notX-slicing

--- codefinity3_data_types.py
+++ codefinity3_data_types.py
@@ -18,6 +18,57 @@ print("The distance between the Venus and the Sun in scientific notation is:", S
 # Define the mass on Earth m1 = 5.972 * 10^24 using scientific notation.
 m1 = 5.972e24
 
+# 2) Complex numbers: 
+number1 = 167 - 98j
+number2 = 67 + 9j
+number3 = 186282
+
+# Print the real part of the number1 variable
+print("The real part of the number1 =", number1.real)
+# Print the imaginary part of the number2 variable
+print("The imaginary part of the number2 =", number2.imag)
+# Print the number 3 variable in a complex form
+print("The speed of light =", number3.real + number3.imag * 1j)
+
+
+
+# 3) Boolean, not X: 
+# This statement equals False
+statement1 = not True or False
+# This statement equals True
+statement2 = True and not False
+
+# Print the first statement
+print(statement1)
+# Print the second statement
+print(statement2)
+
+
+# 4) 
+# Make these statements equal True
+statement1 = True or (True and False)       # wrote the first True
+statement2 = (False and (True and True)) or True    # wrote the last True
+statement3 = ((False or False) or True) and True    # wrote the last True
+
+# Print the first statement
+print(statement1)
+# Print the second statement
+print(statement2)
+# Print the third statement
+print(statement3)
+
+
+# 5) step in slicing, methods find and index, slicing the word
+string = "Life is like riding a bicycle. To keep your balance, you must keep moving"
+sliced_string = string[1:11:4]
+print(sliced_string)    # print "iii" – it takes the first and other fourth symbol
+print('The last symbol:', string[len(string)-1])
+print(string.find('L'))     # find the index of the letter
+print(string.index('is'))   # find the index of the word (otherwise throws an exeptation)
+print(string[string.index('riding'):string.index('riding')+6])   # print word "riding"
+new_string1 = string.replace('bicycle','horse')     # replace the word "bicycle" to "horse"
+print(new_string1)
+
 
 
 
