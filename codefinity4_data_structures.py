967602797be792ac5adbd9fb9e32f67450a4e474
ivjel <jelinkova.ivana@gmail.com>
2022-07-26 22:07:12 +0200

    tuple methods – concatenation, remove, append & set methods – add, update, remove, discard, clear

--- codefinity4_data_structures.py
+++ codefinity4_data_structures.py
@@ -28,6 +28,62 @@ capitals.popitem()  # delete the last pair in dict
 capitals.clear()    # removes all elements in dict
 
 
+# 3) Tuple: 
+
+# Creating tuples – fixed data type
+tuple_1 = (10, 20, 30, 40, 50)
+tuple_2 = (60, 70, 80, 90, 100)
+# second element of tuple_1
+print(tuple_1[1])
+# Concatenation of tuples
+tuple_3 = tuple_1 + tuple_2
+print(tuple_3)
+del tuple_3
+
+# deleting some elements from tuple:
+fruits = ('apple', 'apricot', 'banana', 'grape', 'mango', 'peach', 'pineapple')
+# Turn the tuple into a list
+list_fruits = list(fruits)
+print(list_fruits)
+# Delete items
+list_fruits.remove('banana')
+list_fruits.remove('grape')
+list_fruits.remove('peach')
+list_fruits.append('kiwi')
+# Return the list to the tuple
+fruits = tuple(list_fruits)
+another_tuple = ('orange', 'plum')
+# Concatenate tuples
+fruits += another_tuple     # concatenate the tuples
+# Print the result
+print(fruits)
+
+
+# 4) Set:       Mutable, only unique elements, the order of elements is undefined (pořadí není), different types
+# Creating a set which contains strings
+set_1 = {'pineapple', 'pear', 'cherry'}
+print(set_1)
+# Creating a set which contains a mixed type of values 
+set_2 = {'pear', 45, None, 'car', 'Joe', 12, 5}
+set_2.add(22)       # add an element
+print(set_2)
+set_1.update([20, 25, 100])     # add more elements
+print("lemon" in set_1)     # check if "lemon" is in set
+set_2.remove(45)        # remove 5 from set
+set_2.discard('Joe')
+print(set_2)
+set_1.clear()       # delete all the elements from set
+print(set_1)
+
+
+
+
+
+
+
+
+
+
 
 
 
