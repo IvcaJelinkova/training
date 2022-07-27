36e28e676d0b12f86843b551b05149e651620ca4
ivjel <jelinkova.ivana@gmail.com>
2022-07-27 16:09:56 +0200

    nested loop – invert triangle, matrix

--- codefinity5_loops.py
+++ codefinity5_loops.py
@@ -5,14 +5,14 @@
 # Decreasing --> negative step :-)
 # Printing numbers from 10 to 0
 for i in range(10, -1, -1):
-  print(i)
+  print(i, end=' ')
 print()
 
 numbers = [2, 3, 4, 8, 11]
 counter = 0
 for i in reversed(numbers):     # Printing all elements of the reversed list
     counter += i
-    print(i)
+    print(i, end=' ')
 print('Count:' + str(counter) + '\n')
 
 # 1.2) break, continue, pass of loop 
@@ -57,7 +57,7 @@ print('_______________________________________________________________')
 i = 5
 
 while i > -1:
-  print(i)
+  print(i, end=' ')
   # Decrease 'i'
   i -= 1
 
@@ -90,6 +90,154 @@ while i < len(numbers)-1:
     break       # Finish the loop
   else:
     continue        # Continue searching
+
+
+# 2.3) Print alphabets only: 
+# Here you have a song name. But it is messed with numbers. Remove numbers from these song names
+song_name = 'C233om443123eT9439og4343e77t3484h77e566r'
+i = -1
+
+# Set 'while' loop to work with 'song_name'
+while i < len(song_name)-1:
+  # Increase 'i'
+  i += 1
+  # Check if an element is not alpha
+  if not song_name[i].isalpha():
+    continue
+  else:
+    # Print an element
+    print(song_name[i], end=' ')
+
+
+print('\n_______________________________________________________________')
+# 3) Nested loops: 
+# 3.1) invert triangle: 
+# Set outer loop for the number of rows
+for i in range(4, 0, -1):
+  # Set inner loop for the number of elements in the row
+  for j in range(1, i + 1):
+    # Print '*'
+    print('*', end=' ')
+  print('')
+print()
+
+# 3.2) MATRIX with for: 
+# You need to sum all elements of the matrix.
+matrix = [ [1, 2, 4, 29],
+           [3, 4, 6, 1] ]
+
+counter = 0
+# Set outer loop to work with the number of rows
+for i in range(len(matrix)):
+  # Inner loop to work with the number of element in the row
+  for j in range(len(matrix[i])):
+    counter += matrix[i][j]
+
+print(counter)
+
+
+# MATRIX with while: 
+# Add 1 to every element of the matrix.
+matrix = [ [1, 2, 4, 29],
+           [3, 4, 6, 1],
+          [10, -5, 0]]
+
+# Set outer 'while' loop to work with the number of rows
+i = 0
+while i < len(matrix):
+  # Set inner 'while' loop to work with the number of elements in the row
+  j = 0
+  while j < len(matrix[i]):
+    # Change an element
+    matrix[i][j] += 1
+    # Print an element
+    print(matrix[i][j], end = ' ')
+    j +=1
+  i += 1
+  print('')
+
+
+# Print all letters from the 'text' which are vowels.
+text = 'ofsddsfadfjfojnanjinjudfninvjunjee'
+vowels = 'aeiou'
+
+# Set 'i'
+i = 0
+# Set 'while' loop to work with elements of the 'text'
+while i < len(text):
+  # Set 'for' loop to work with the elements of the 'vowels'
+  for j in range(len(vowels)):
+    # If an element in the 'text' is one of the elements in 'vowels'
+    if text[i] == vowels[j]:
+      # Print an element
+      print(text[i], end=' ')
+  i += 1
+
+print('\n_________')
+# my little bit shorter solution: :-) 
+for character in text: 
+  if character in vowels: 
+    print(character, end=' ')
+print()
+
+
+
+# You need to clean the list given from symbols and numbers. 
+matrix = [[' L', '#', 'o', 'o', '#', 's', 'i', 'n', 'g', ' ', '#', 'm', 'y '],
+  ['r', 'e', '#', 'l', 'i','#', 'g', 'i', 'o', '#', 'n', '!', 'apspsasd']]
+
+# Set 'for' loop to work with the number of rows 
+for i in range(len(matrix)):
+  # Set 'for' loop to work with the number of elements of the row
+  for j in range(len(matrix[i])):
+    # Set condition if an element is '#'
+    if matrix[i][j] == '#':
+      continue
+    # Set condition if an element is '!'
+    elif matrix[i][j] == '!':
+      break
+    else:
+      # Print an element
+      print(matrix[i][j], end=' ')
+print()
+
+
+# Clasify to vowels, consonants and symbols: 
+text = 'aew$^&hg32yy8wer3$#^*@#7ds983hn'
+vowels = 'aeiou'
+
+text_vowels = ''
+text_consonants = ''
+text_symbols = ''
+
+# Set 'i'
+i = 0
+# Set outer *while* loop to work with 'text'
+while i < len(text):
+  # Set 'u'
+  u = 0
+  # Set condition *if an element is a letter*
+  if text[i].isalpha():
+    # Set inner *while* loop to work with 'vowels'
+    while u < len(vowels):
+      # Set condition *if a letter from the 'text' is in the vowels'* 
+      if text[i] == vowels[u]:
+        # Add the letter to the 'text_vowels'
+        text_vowels += text[i]
+        break
+      # Increase 'u'
+      u += 1
+    else:
+      # Add the letter to the 'text_consonants'
+      text_consonants += text[i]
+  else:
+    # Add the letter to the 'text_symbols'
+    text_symbols += text[i]
+  i += 1
+  
+print('Vowels from the text: ', text_vowels)
+print('Consonants from the text: ', text_consonants)
+print('Symbols from the text: ', text_symbols)
 
 
 
